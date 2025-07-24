d8a0e21b07733d51c70abbf9a489684b9c0e9716
AiRa0908 <aira01100908@gmail.com>
2025-07-25 00:33:32 +0900

    アイテムの編集

--- app/api/item/readsingle/[id]/route.js
+++ app/api/item/readsingle/[id]/route.js
@@ -4,7 +4,7 @@ import supabase from "@/app/utils/database";
 export async function GET(request, context){
     const params = await context.params
     try{
-        const {data, error } = await supabase.from("items").select().eq("id", params.id).single()//.single()は配列で渡されるデータを単一のオブジェクト形式に変換する
+        const {data, error} = await supabase.from("items").select().eq("id", params.id).single()//.single()は配列で渡されるデータを単一のオブジェクト形式に変換する
         if(error) throw new Error(error.message)
         return NextResponse.json({message: "アイテム読み取り成功（シングル）", singleItem: data})
     }catch(err){
